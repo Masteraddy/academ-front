e46c44c4ba6ef9b6b9b5d49b56f6dd01cc6298b7
Masteraddy <adesileisaiah70@gmail.com>
2021-01-29 16:45:14 +0100

    Improve Dash

--- components/Pages/Dashboard.js
+++ components/Pages/Dashboard.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Card, Col, Row, Statistic } from 'antd';
 import Link from 'next/link';
-import routes from '../../libs/routes';
-
-const data = [
-  {
-    path: '/school/roles',
-    name: 'Roles',
-  },
-  {
-    path: '/school/staffs',
-    name: 'Staffs',
-  },
-];
+// import routes from '../../libs/routes';
+// import store from '../../redux'
+import { useStoreState } from 'easy-peasy';
 
 const Dashboard = () => {
+
+  const roles = useStoreState((o) => o.roles.roles);
+  const staffs = useStoreState((o) => o.staffs.staffs);
+
+  const data = [
+    {
+      path: '/school/roles',
+      name: 'Roles',
+      stat: roles.length
+    },
+    {
+      path: '/school/staffs',
+      name: 'Staffs',
+      stat: staffs.length
+    },
+  ]
+
   return (
     <div style={{ minHeight: '80vh' }}>
       <Row gutter={12}>
@@ -22,7 +30,7 @@ const Dashboard = () => {
           <Col span={12} style={{ marginBottom: 12 }}>
             <Link href={e.path}>
               <Card hoverable style={{ textAlign: 'center' }}>
-                <Statistic title={e.name} value={0} />
+                <Statistic title={e.name} value={e.stat} />
               </Card>
             </Link>
           </Col>
